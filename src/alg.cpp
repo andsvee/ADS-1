167f71d851b3d740c60a727ecef21a9fc7e0952a
andsvee <160344384+andsvee@users.noreply.github.com>
2024-03-02 00:39:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,55 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+    uint64_t count = 0;
+    uint64_t num = 2;
+    while (count < n) {
+        if (checkPrime(num)) {
+            ++count;
+        }
+        ++num;
+    }
+    return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+    uint64_t next = value + 1;
+    while (true) {
+        if (checkPrime(next)) {
+            return next;
+        }
+        next++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t num = 2; num < hbound; num++) {
+        bool isPrime = true;
+            for (uint64_t divs = 2; divs <= num / 2; divs++) {
+                if (num % divs == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        if (isPrime) {
+            sum += num;
+        }
+    }
+    return sum;
 }
